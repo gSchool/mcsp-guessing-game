7fe03dd2b1be488326d56b560147d7d47d271cd2
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-01 14:39:24 -0600

    finsied

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-play(); // This is your main function that runs when the page loads
-
+ // This is your main function that runs when the page loads
+var secret = 99;
 function play(){
-   
+   var guess = prompt('guess a number');
+   var numberGuess = parseInt(guess);
+   console.log(guess);
+   if (guess === numberGuess) {
+       alert('you got it rght');
+   } else {
+       alert('you got it wrong');
+   }
 }
 
 function playAgain(){
     
 }
+
+play();
