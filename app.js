b09d8ab20fa14c728a766030652e3dba6aa1cc73
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-01 15:30:33 -0600

    mahor update

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
  // This is your main function that runs when the page loads
-var secret = 99;
-var random = Math.floor(Math.random() * 100)
 
+// var random = Math.floor(Math.random() * 100)
+var count = 1;
+var scoreArr = [];
+var i = 0;
 function randomInt(max) {
-    return Math.floor(math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 var randomInput = randomInt(100);
+var secretNumber = randomInput
+console.log(secretNumber);
+
 function play(){
-   var guess = prompt('guess a number');
-   var numberGuess = parseInt(guess);
+   var playerName = prompt('What is your name?')
+   var guess = prompt('Welcome, ' + playerName + '! Guess a number!');
+   var numberGuess = Number(guess);
    console.log(guess);
-   if (guess === numberGuess) {
-       alert('you got it rght');
-   } else {
-       alert('you got it wrong');
+   if (numberGuess === secretNumber) {
+       if (count === 0) {
+           alert("Correct! It only took you one guess!");
+       } else {
+           alert("Correct! It only took you " + count +  " guesses!" + '\n' + "Your previous guesses were: " + scoreArr.join(', '));
+       }
+       
+       count = 1;
+       
+   } else if (numberGuess > secretNumber) {
+       alert('Guess Lower');
+       count++
+       scoreArr[i] = numberGuess
+       i++
+       play();
+   } else if( numberGuess < secretNumber) {
+       alert('Guess Higher');
+       count++
+       scoreArr[i] = numberGuess
+       i++
+       play();
    }
 }
 
