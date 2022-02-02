a1d641b5a31ad2a6ff8d7784f951bdbf0037662b
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-02 12:30:54 -0600

    finished

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
 //Starting variables
-
 
 //current amount of guesses
 var count = 1;
-//guesses taken in the current iteration 
+//guesses taken in the current iteration
 var scoreArr = [];
 //iterating variable through the current guess array
 var i = 0;
@@ -12,53 +10,68 @@ var i = 0;
 var playerName = "";
 //tells what the last playthrough score was
 var lastPlay = 0;
-//creates empty object that player name and associated scores will live in 
+//creates empty object that player name and associated scores will live in
 var playerObj = {};
-
 
 //determine secret number
 function randomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-var randomInput = randomInt(100);
+//assign secret number to variable we will change later
+var randomInput = randomInt(101);
 var secretNumber = randomInput;
+//console log for testing purposes.
 console.log(secretNumber);
+//this marker tells if the current player is the play who started this instance of the game
+var currentPlayerMarker = 0;
 
-var marker = 0;
-
+//function to start at launch
 function play() {
+  /*this temp variable is used to determine if the current score is better than any score
+    in the player's array in the player object*/
   var temp = Infinity;
-  if (marker === 0) {
+  //if not a continued instance
+  if (currentPlayerMarker === 0) {
     var localPlayerName = prompt("What is your name?");
     playerName = localPlayerName;
+    //determines if player has played before or not by referencing playerObj
     if (playerObj[playerName] === undefined) {
       playerObj[playerName] = [];
     } else {
-        for (int of playerObj[playerName]) {
-            if (int < temp) {
-              temp = int;
-            }
-          }
-        alert("Welcome back, " + localPlayerName + "! Your previous best score is " + temp + "!");
+      //iterates over stored array in the object to find lowest score
+      for (int of playerObj[playerName]) {
+        if (int < temp) {
+          temp = int;
+        }
+      }
+      alert(
+        "Welcome back, " +
+          localPlayerName +
+          "! Your previous best score is " +
+          temp +
+          "!"
+      );
     }
-    // playerObj[playerName] = playerName;
-    
+    //makes sure the name is a valid string
     if (playerName === null || playerName === undefined || playerName === "") {
       playerName = prompt("Please enter your name");
-      marker = 0;
+      currentPlayerMarker = 0;
       play();
     }
-    var guess = prompt("Welcome, " + localPlayerName + "! Guess a number!");
+    var guess = prompt("Welcome, " + localPlayerName + "! Guess a number between 1 and 100!");
     console.log(playerObj);
-    marker++;
+    currentPlayerMarker++;
   } else {
-    var guess = prompt("Guess a number!");
+    var guess = prompt("Guess a number between 1 and 100!");
   }
-
+  //turns string from input into number
   var numberGuess = Number(guess);
+  //console for testing
   console.log(guess);
+  //if correct number is guessed
   if (numberGuess === secretNumber) {
+    //if only one guess
     if (count === 1) {
       playerObj[playerName].push(count);
       if (playerObj[playerName].length === 1) {
@@ -66,6 +79,7 @@ function play() {
           "Correct! It only took you one guess! This is the best score possible!"
         );
       } else {
+        //if only one guess and its not the first iteration.
         for (int of playerObj[playerName]) {
           if (int < temp) {
             temp = int;
@@ -76,8 +90,9 @@ function play() {
           "Correct! It only took you one guess! Your best score is: " + temp
         );
       }
-
+      //asks if the player wants to play again. anything other than yes will result in the game ending
       var again = prompt("Play again? 'Yes' / 'No'");
+      //covers cases of any variation of the word yes
       var sterileAgain = again.toLowerCase();
       if (sterileAgain === "yes") {
         var newPlayer = prompt("New Player? 'Yes / No'");
@@ -89,56 +104,26 @@ function play() {
           samePlayer();
         }
       }
-    } else if (count === 2){
-        for (int of playerObj[playerName]) {
-            if (int < temp) {
-              temp = int;
-            }
-          }
-          if (temp === Infinity) {
-              temp = count;
-          } else if(count < temp) {
-              temp = count;
-          }
-          alert(
-            "Correct! It only took you two guesses!" +
-              "\n" +
-              "Your previous guess was: " +
-              scoreArr.join(", ") + "\n" + "Your best score is " + temp 
-          );
-    
-          playerObj[playerName].push(count);
-          console.log(playerObj);
-          again = prompt("Play again? 'Yes' / 'No'");
-          sterileAgain = again.toLowerCase();
-          if (sterileAgain === "yes") {
-            var newPlayer = prompt("New Player? 'Yes / No'");
-            var sterileNewPlayer = newPlayer.toLowerCase();
-            if (sterileNewPlayer === "yes") {
-              playAgain();
-            } else {
-              samePlayer();
-            }
-          } 
-    }else {
-        
+      //same as above only changing wording to account for it only taking two guesses.
+    } else if (count === 2) {
       for (int of playerObj[playerName]) {
         if (int < temp) {
           temp = int;
         }
       }
       if (temp === Infinity) {
-          temp = count;
-      } else if(count < temp) {
-          temp = count;
+        temp = count;
+      } else if (count < temp) {
+        temp = count;
       }
       alert(
-        "Correct! It only took you " +
-          count +
-          " guesses!" +
+        "Correct! It only took you two guesses!" +
           "\n" +
-          "Your previous guesses were: " +
-          scoreArr.join(", ") + "\n" + "Your best score is " + temp 
+          "Your previous guess was: " +
+          scoreArr.join(", ") +
+          "\n" +
+          "Your best score is " +
+          temp
       );
 
       playerObj[playerName].push(count);
@@ -153,9 +138,45 @@ function play() {
         } else {
           samePlayer();
         }
-      } 
-    }
+      }
+    } else {
+      for (int of playerObj[playerName]) {
+        if (int < temp) {
+          temp = int;
+        }
+      }
+      if (temp === Infinity) {
+        temp = count;
+      } else if (count < temp) {
+        temp = count;
+      }
+      alert(
+        "Correct! It only took you " +
+          count +
+          " guesses!" +
+          "\n" +
+          "Your previous guesses were: " +
+          scoreArr.join(", ") +
+          "\n" +
+          "Your best score is " +
+          temp
+      );
 
+      playerObj[playerName].push(count);
+      console.log(playerObj);
+      again = prompt("Play again? 'Yes' / 'No'");
+      sterileAgain = again.toLowerCase();
+      if (sterileAgain === "yes") {
+        var newPlayer = prompt("New Player? 'Yes / No'");
+        var sterileNewPlayer = newPlayer.toLowerCase();
+        if (sterileNewPlayer === "yes") {
+          playAgain();
+        } else {
+          samePlayer();
+        }
+      }
+    }
+    //main part of the game are these two statements
     count = 1;
   } else if (numberGuess > secretNumber) {
     alert("Sorry, " + playerName + ". Guess Lower");
@@ -171,13 +192,13 @@ function play() {
     play();
   }
 }
-
+//functions to reduce repeat code
 function playAgain() {
-  marker = 0;
+  currentPlayerMarker = 0;
   count = 1;
   i = 0;
   scoreArr = [];
-  randomInput = randomInt(100);
+  randomInput = randomInt(101);
   secretNumber = randomInput;
   console.log(secretNumber);
   play();
@@ -187,10 +208,24 @@ function samePlayer() {
   count = 1;
   i = 0;
   scoreArr = [];
-  randomInput = randomInt(100);
+  randomInput = randomInt(101);
   secretNumber = randomInput;
   console.log(secretNumber);
   play();
 }
-
+function endGame() {
+  playerObj[playerName].push(count);
+  console.log(playerObj);
+  again = prompt("Play again? 'Yes' / 'No'");
+  sterileAgain = again.toLowerCase();
+  if (sterileAgain === "yes") {
+    var newPlayer = prompt("New Player? 'Yes / No'");
+    var sterileNewPlayer = newPlayer.toLowerCase();
+    if (sterileNewPlayer === "yes") {
+      playAgain();
+    } else {
+      samePlayer();
+    }
+  }
+}
 play();
