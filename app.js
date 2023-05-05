005c7711647ac01a49ee3dc90ffe71f3696d19b1
DylanWGordon <dw.gordon2@gmail.com>
2023-05-05 12:33:11 -0700

    Names v1

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let secret = Math.floor(Math.random() * 100);
 let counter = 0;
+function typeName(){
+    prompt('Enter your name')
+}
+typeName();
+let myName = typeName;
+
 function guessMe(num) {
   let attempt = Number(prompt("What is your number?"));
   if (num === attempt) {
-    alert(`Correct! It only took you ${counter} guesses!`);
+    alert(`Correct, ${myName}! It only took you ${counter} guesses!`);
   } else if (num < attempt) {
-    alert("Lower");
+    alert(`Sorry, ${myName}. Try a lower number.`);
     counter++;
     guessMe(num);
   } else if (num > attempt) {
-    alert("Higher");
+    alert(`Sorry, ${myName}. Try a higher number.`);
     counter++;
     guessMe(num);
   }
